2e888f8b2f014b4e82feb2959ffd19e5a8681836
Brendan Sheehan <feemcgill@gmail.com>
2020-08-20 11:48:22 -0700

    adding legal and contact deets

---
+++ components/ContactDeets.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="deets">
+    <ul class="mains">
+      <li><a href="#">323.405.7545</a></li>
+      <li><a href="#">hello@devgru.la</a></li>
+    </ul>
+    <ul class="seconds">
+      <li><a href="#">Instagram</a></li>
+      <li><a href="#">Twitter</a></li>
+      <li><a href="#">LinkedIn</a></li>          
+    </ul>    
+  </div>
+</template>
+<style lang="scss" scoped>
+  .deets {
+    text-align: center;
+    .mains {
+      font-size: 2em;
+      margin-bottom: 0.5em;
+    }
+    .seconds {
+      font-size: 1.5em;
+    }
+    a {
+      color: $flair;
+      text-decoration: none;
+    }  
+  }
+</style>
---
+++ components/Legal.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container legal">
+    &copy;2020 Development Group. All Rights Reserved.
+  </div>
+</template>
+<style lang="scss" scoped>
+  .legal {
+    font-size: 0.7em;
+    text-align: center;
+    color: $flair;
+  }
+</style>
