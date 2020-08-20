043e371291faa0ba00cdc2209ef7df1f719c8b62
Brendan Sheehan <feemcgill@gmail.com>
2020-08-20 13:19:29 -0700

    add more graphix, homepage start

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="container">
-    <h1>devgru</h1>
+  <div class="home">
+    <div class="stage">
+      <figure draggable="true" class="logo" />
+      <figure draggable="true" class="nuthin" />
+    </div>
   </div>
 </template>
 
@@ -15,3 +18,32 @@ export default {
   // }
 }
 </script>
+
+<style lang="scss" scoped>
+  .stage {
+    background: $white;
+    position: absolute;
+    height: 100vh;
+    width: 100vw;
+    figure {
+      position: absolute;
+      background-repeat: no-repeat;
+      /* background-color: yellow; */
+    }
+    .logo {
+      width: 50vh;
+      padding-bottom: 12%;
+      top: 30px;
+      left: 100px;
+      background-image: url('~assets/graphix/devgru-logo.svg');
+    }
+
+    .nuthin {
+      width: 20vh;
+      padding-bottom: 22%;
+      top: 300px;
+      left: 700px;
+      background-image: url('~assets/graphix/nuthin.svg');
+    }    
+  }
+</style>
