8cc641551349e4f9575930643b22d27dbe72e422
Mike Watt <watt.mike@gmail.com>
2020-08-20 17:32:02 -0500

    some progress on capabilities page

--- pages/capabilities.vue
+++ pages/capabilities.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <div id="matter-js">
+  <div v-if="!$apollo.loading && page">
+    <!-- <span class="shadow" v-html="page.CapabilitiesFields.wordGroup1"></span> -->
+    <div id="capabilities-matter-js">
       
     </div>
   </div>
@@ -26,35 +27,12 @@ import {
 } from 'matter-js'
 import decomp from 'poly-decomp'
 
-// export default {
-//   apollo: { 
-//     page: {
-//       query: gql`
-//       `,
-//     }
-//   }
-// }
 export default {
   data: function() {
     return {
-      wordsArr1: [
-        'HTML/CSS',
-        'Javascript',
-        'jQuery',
-        'Javascript Frameworks',
-        'Front End Frameworks',
-        'CSS Preprocessors',
-        'RESTful Services / APIs',
-        'Responsive / Mobile Design',
-        'Cross-Browser Development',
-        'Content Management Systems',
-        'Testing / Debugging',
-        'Git / Version Control',
-        'Problem Solving',
-        'Being Smart'
-      ],
-      wordsArr2: ["Shoulder", "turducken", "fatback", "pork", "loin", "bresaola", "pastrami", "ball", "tip.", "Cupim", "shank", "ribeye", "turkey", "capicola", "buffalo", "chislic", "drumstick", "andouille", "doner", "pig", "porchetta", "spare", "ribs", "fatback.", "Jerky", "tongue", "ball", "tip", "kevin", "pancetta", "tri-tip", "jowl", "beef", "ribs", "pork", "shoulder", "salami", "meatloaf", "short", "loin.", "Ground", "round", "t-bone", "chislic", "shankle", "tenderloin", "short", "ribs", "capicola", "prosciutto", "frankfurter", "pork", "loin."],
-      wordsArr3: ["Lorizzle", "ipsum", "hizzle", "amizzle,", "consectetuer", "adipiscing", "tellivizzle.", "Nullam", "sapizzle", "velizzle,", "yippiyo", "volutpizzle,", "da", "bomb", "boom", "shackalack,", "fo", "shizzle", "vel,", "arcu.", "Pellentesque", "fizzle", "tortizzle.", "Check", "out", "this", "erizzle.", "Fo", "shizzle", "izzle", "for", "sure", "dapibizzle", "turpis", "tempus", "yippiyo.", "Maurizzle", "pellentesque", "nibh", "et", "turpizzle.", "Uhuh", "...", "yih!", "hizzle", "i", "saw", "beyonces", "tizzles", "and", "my", "pizzle", "went", "crizzle.", "Pellentesque", "cool", "rhoncizzle", "hizzle.", "In", "pot", "habitasse", "platea", "dictumst.", "Cool", "dapibus.", "Curabitizzle", "sheezy", "the", "bizzle,", "pretium", "eu,", "mattis", "fo", "shizzle,", "eleifend", "vitae,", "pot.", "I'm", "in", "the", "shizzle", "suscipizzle.", "Integizzle", "shut", "the", "shizzle", "up", "ass", "sizzle", "crackalackin."],
+      resetMatterDelayTimer: null,
+      engine: null,
+      render: null,
     };
   },
   computed: {
@@ -98,146 +76,227 @@ export default {
       }
 
       return imgObj
-    }
-  },
-  mounted() {
-    if (process.client) {
+    },
+    shuffleArr(a) {
+      for (let i = a.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [a[i], a[j]] = [a[j], a[i]];
+      }
+      return a;
+    },
+    onResize(e) {
+      clearTimeout(this.resetMatterDelayTimer)
+      this.resetMatterDelayTimer = setTimeout( (e) => {
+        this.destroyMatterJS()
+        this.setupMatterJS()
+      }, 500)
+    },
+    setupMatterJS() {
+      let width = window.innerWidth
+      let height = window.innerHeight
 
-      // force fonts to load before rendering canvas
-      document.fonts.load('20px "ITC Garamond"').then(()=> {
-        
-        let width = window.innerWidth
-        let height = window.innerHeight - 55
+      // create an engine
+      this.engine = Engine.create()
 
-        // create an engine
-        let engine = Engine.create()
+      let renderOptions = {
+        width,
+        height,
+        wireframes: false,
+        wireframeBackground: "transparent",
+        // background: "transparent",
+        background: "#ffff99",
+        showSleeping: true,
+        showDebug: false,
+        showBroadphase: false,
+        showBounds: false,
+        showVelocity: false,
+        showCollisions: false,
+        showSeparations: false,
+        showAxes: false,
+        showAngleIndicator: false,
+        showIds: false,
+        showShadows: false,
+        showVertexNumbers: false,
+        showConvexHulls: false,
+        showInternalEdges: false
+      }
 
-        let renderOptions = {
-          width,
-          height,
-          wireframes: false,
-          wireframeBackground: "transparent",
-          // background: "transparent",
-          background: "#ffff99",
-          showSleeping: true,
-          showDebug: false,
-          showBroadphase: false,
-          showBounds: false,
-          showVelocity: false,
-          showCollisions: false,
-          showSeparations: false,
-          showAxes: false,
-          showAngleIndicator: false,
-          showIds: false,
-          showShadows: false,
-          showVertexNumbers: false,
-          showConvexHulls: false,
-          showInternalEdges: false
-        }
+      // create a renderer
+      this.render = Render.create({
+          element: document.querySelector("#capabilities-matter-js"),
+          engine: this.engine,
+          options: renderOptions
+      })
 
-        // create a renderer
-        let render = Render.create({
-            element: document.querySelector("#matter-js"),
-            engine: engine,
-            options: renderOptions
-        })
+      // create bodies
+      let bodiesArr = [];
+      let wallThickness = 1000
+      let wallLength = 10000
+      // let boxA =        Bodies.rectangle(400, 200, 80, 80)
+      // let boxB =        Bodies.rectangle(450, 50, 180, 80)
+      let ceiling =     Bodies.rectangle(width / 2, -wallThickness, width, wallThickness, { isStatic: true })
+      let ground =      Bodies.rectangle(width / 2, height + wallThickness / 2, width, wallThickness, { isStatic: true })
+      let wallLeft =    Bodies.rectangle(-wallThickness / 2, height / 2, wallThickness, wallLength, { isStatic: true })
+      let wallRight =   Bodies.rectangle(width + wallThickness / 2, height / 2, wallThickness, wallLength, { isStatic: true })
+      bodiesArr.push(ground, wallLeft, wallRight);
 
-        // create bodies
-        let bodiesArr = [];
-        let wallThickness = 1000
-        let wallLength = 10000
-        // let boxA =        Bodies.rectangle(400, 200, 80, 80)
-        // let boxB =        Bodies.rectangle(450, 50, 180, 80)
-        let ceiling =     Bodies.rectangle(width / 2, -wallThickness, width, wallThickness, { isStatic: true })
-        let ground =      Bodies.rectangle(width / 2, height + wallThickness / 2, width, wallThickness, { isStatic: true })
-        let wallLeft =    Bodies.rectangle(-wallThickness / 2, height / 2, wallThickness, wallLength, { isStatic: true })
-        let wallRight =   Bodies.rectangle(width + wallThickness / 2, height / 2, wallThickness, wallLength, { isStatic: true })
+      // generate words
+      let yPos = -50;
+      let wordsArr1 = this.page.CapabilitiesFields.wordGroup1.split(',')
+      wordsArr1 = this.shuffleArr(wordsArr1)
+      for (let i = 0; i < wordsArr1.length; i ++) {
+        let randomFontSize = Math.random() * (75 - 30) + 30; 
+        let wordImage = this.createWordImage( wordsArr1[i], randomFontSize );
+        let randomXPos = Math.random() * ((width - wordImage.width) - wordImage.width) + wordImage.width;
+        let randomRotation = Math.random() * (45 - -45) + -45; 
+        let word =      Bodies.rectangle(randomXPos, yPos, wordImage.width, wordImage.height, { render: { sprite: { texture: wordImage.image } } })
+        Body.rotate(word, Math.PI/randomRotation)
+        yPos -= 200
+        bodiesArr.push(word)
+      }
+      
+      // add all of the bodies to the world
+      World.add(this.engine.world, bodiesArr)
 
-        // generate words
-        let yPos = -50;
-        for (let i = 0; i < this.wordsArr1.length; i ++) {
-          let randomFontSize = Math.random() * (75 - 30) + 30; 
-          let wordImage = this.createWordImage( this.wordsArr1[i], randomFontSize );
+      let bodiesArr2 = []
+      let wordsArr2 = this.page.CapabilitiesFields.wordGroup2.split(',')
+      setTimeout(()=> {
+        yPos = -50;
+        for (let i = 0; i < wordsArr2.length; i ++) {
+          let randomFontSize = Math.random() * (50 - 20) + 20; 
+          let wordImage = this.createWordImage( wordsArr2[i], randomFontSize );
           let randomXPos = Math.random() * ((width - wordImage.width) - wordImage.width) + wordImage.width;
           let randomRotation = Math.random() * (45 - -45) + -45; 
           let word =      Bodies.rectangle(randomXPos, yPos, wordImage.width, wordImage.height, { render: { sprite: { texture: wordImage.image } } })
           Body.rotate(word, Math.PI/randomRotation)
           yPos -= 200
-          bodiesArr.push(word)
+          bodiesArr2.push(word)
         }
+        World.add(this.engine.world, bodiesArr2)
+      }, this.page.CapabilitiesFields.delayBeforeWordGroup2 * 1000)
 
-        let bodiesArr2 = []
-        setTimeout(()=> {
-          yPos = -50;
-          for (let i = 0; i < this.wordsArr2.length; i ++) {
-            let randomFontSize = Math.random() * (50 - 20) + 20; 
-            let wordImage = this.createWordImage( this.wordsArr2[i], randomFontSize );
-            let randomXPos = Math.random() * ((width - wordImage.width) - wordImage.width) + wordImage.width;
-            let randomRotation = Math.random() * (45 - -45) + -45; 
-            let word =      Bodies.rectangle(randomXPos, yPos, wordImage.width, wordImage.height, { render: { sprite: { texture: wordImage.image } } })
-            Body.rotate(word, Math.PI/randomRotation)
-            yPos -= 200
-            bodiesArr2.push(word)
-          }
-          World.add(engine.world, bodiesArr2)
-        }, 6000)
+      let bodiesArr3 = []
+      let wordsArr3 = this.page.CapabilitiesFields.wordGroup3.split(',')
+      setTimeout(()=> {
+        yPos = -50;
+        for (let i = 0; i < wordsArr3.length; i ++) {
+          let randomFontSize = Math.random() * (30 - 10) + 10; 
+          let wordImage = this.createWordImage( wordsArr3[i], randomFontSize );
+          let randomXPos = Math.random() * ((width - wordImage.width) - wordImage.width) + wordImage.width;
+          let randomRotation = Math.random() * (45 - -45) + -45; 
+          let word =      Bodies.rectangle(randomXPos, yPos, wordImage.width, wordImage.height, { render: { sprite: { texture: wordImage.image } } })
+          Body.rotate(word, Math.PI/randomRotation)
+          yPos -= 200
+          bodiesArr3.push(word)
+        }
+        World.add(this.engine.world, bodiesArr3)
+      }, this.page.CapabilitiesFields.delayBeforeWordGroup3 * 1000)
 
-        let bodiesArr3 = []
-        setTimeout(()=> {
-          yPos = -50;
-          for (let i = 0; i < this.wordsArr3.length; i ++) {
-            let randomFontSize = Math.random() * (30 - 10) + 10; 
-            let wordImage = this.createWordImage( this.wordsArr3[i], randomFontSize );
-            let randomXPos = Math.random() * ((width - wordImage.width) - wordImage.width) + wordImage.width;
-            let randomRotation = Math.random() * (45 - -45) + -45; 
-            let word =      Bodies.rectangle(randomXPos, yPos, wordImage.width, wordImage.height, { render: { sprite: { texture: wordImage.image } } })
-            Body.rotate(word, Math.PI/randomRotation)
-            yPos -= 200
-            bodiesArr3.push(word)
-          }
-          World.add(engine.world, bodiesArr3)
-        }, 12000)
+      // Final word
+      setTimeout(()=> {
+        let wordImage = this.createWordImage( this.page.CapabilitiesFields.bigWord, width / 6 );
+        let word =      Bodies.rectangle(width / 2, -300, wordImage.width, wordImage.height, { render: { sprite: { texture: wordImage.image } } })
+        World.add(this.engine.world, [word])
+      }, this.page.CapabilitiesFields.delayBeforeBigWord * 1000)
 
-        // Final word
-        setTimeout(()=> {
-          let wordImage = this.createWordImage( 'devgru', 250 );
-          let word =      Bodies.rectangle(width / 2, -300, wordImage.width, wordImage.height, { render: { sprite: { texture: wordImage.image } } })
-          World.add(engine.world, [word])
-        }, 12000)
 
-        bodiesArr.push(ground, wallLeft, wallRight);
+      // add mouse control
+      let mouse = Mouse.create(this.render.canvas),
+        mouseConstraint = MouseConstraint.create(this.engine, {
+            mouse: mouse,
+            constraint: {
+                stiffness: 0.2,
+                render: {
+                    visible: false
+                }
+            }
+        });
 
-        // add all of the bodies to the world
-        World.add(engine.world, bodiesArr)
+      // re-enable mouse scrolling
+      mouse.element.removeEventListener("mousewheel", mouse.mousewheel)
+      mouse.element.removeEventListener("DOMMouseScroll", mouse.mousewheel)
 
-        // add mouse control
-        let mouse = Mouse.create(render.canvas),
-          mouseConstraint = MouseConstraint.create(engine, {
-              mouse: mouse,
-              constraint: {
-                  stiffness: 0.2,
-                  render: {
-                      visible: false
-                  }
-              }
-          });
+      World.add(this.engine.world, mouseConstraint)
+      // engine.world.gravity.scale = 0.0005
 
-        // re-enable mouse scrolling
-        mouse.element.removeEventListener("mousewheel", mouse.mousewheel)
-        mouse.element.removeEventListener("DOMMouseScroll", mouse.mousewheel)
+      // keep the mouse in sync with rendering
+      this.render.mouse = mouse
 
-        World.add(engine.world, mouseConstraint)
-        engine.world.gravity.scale = 0.0005
+      // run the engine
+      Engine.run(this.engine)
 
-        // keep the mouse in sync with rendering
-        render.mouse = mouse
+      // run the renderer
+      Render.run(this.render)
+    },
+    destroyMatterJS() {
+      if (this.engine != null) {
+        World.clear(this.engine.world);
+        Engine.clear(this.engine);
+        Render.stop(this.render);
+        this.render.canvas.remove();
+        this.render.canvas = null;
+        this.render.context = null;
+        this.render.textures = {};
+      }
+    }
+  },
+  mounted() {
+    console.log('capabilities: MOUNTED')
+    if (process.client) {
 
-        // run the engine
-        Engine.run(engine)
+      // remove scrollbars
+      document.getElementsByTagName('body')[0].style.overflow = 'hidden'
 
-        // run the renderer
-        Render.run(render)
+      // add resize listeners
+      window.addEventListener('resize', this.onResize)
+
+      // force fonts to load before rendering canvas
+      document.fonts.load('20px "ITC Garamond"').then(()=> {
+        
+        if (this.page) {
+          this.setupMatterJS()
+        }
+        
       });
+    }
+  },
+  updated() {
+    console.log('capabilities: UPDATED')
+      // force fonts to load before rendering canvas
+      document.fonts.load('20px "ITC Garamond"').then(()=> {
+        
+        if (this.page) {
+          this.destroyMatterJS()
+          this.setupMatterJS()
+        }
+        
+      });
+  },
+  beforeDestroy() {
+    this.destroyMatterJS()
+    document.getElementsByTagName('body')[0].style.overflow = 'initial'
+  },
+  apollo: { 
+    page: {
+      error: function(error) {
+        console.log(error)
+      },
+      query: gql`
+        query CapabilitiesQuery {
+          page(id: "100", idType: DATABASE_ID) {
+            title                 
+            CapabilitiesFields {
+              wordGroup1
+              wordGroup2
+              wordGroup3
+              delayBeforeWordGroup3
+              delayBeforeWordGroup2
+              delayBeforeBigWord
+              bigWord
+            }
+          }
+        } 
+      `,
     }
   }
 }
@@ -246,9 +305,13 @@ export default {
 
 
 <style lang="scss">
-#matter-js {
+#capabilities-matter-js {
+
   canvas {
-    
-  }
+    width: 100vw;
+    height: 100vh;
+  }  
 }
+
+
 </style>
