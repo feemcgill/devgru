32d8427180151dfcbc889f5e4058e2c712810634
Mike Watt <watt.mike@gmail.com>
2021-08-27 13:22:54 -0500

    roughed in shooter game

--- pages/shooter.vue
+++ pages/shooter.vue
@@ -1,67 +1,135 @@
-<template>
-  
-</template>
+<template> </template>
 
 <script>
-
 export default {
   data: function() {
     return {
-
+      k: kaboom({
+        global: true,
+        // debug: true,
+        width: document.body.clientWidth,
+        height: window.innerHeight - 4
+      }),
+      assets_enemies: [
+        "bomb",
+        "computer",
+        "documents",
+        "folder",
+        "peace",
+        "shout",
+        "star-circle",
+        "thumbs-up"
+      ],
+      assets_friends: ["keyboard"],
+      player: null,
+      num_assets_loaded: 0,
+      num_total_assets: null,
+      game_started: false,
+      PLAYER_SPEED: 220,
+      ENEMY_SPEED: 50,
+      ENEMY_HEALTH: 4
     }
   },
-  computed: {
-    
-  },
-  methods: {
-  },
+  computed: {},
   mounted() {
-    console.log('shooter: MOUNTED')
+    // Load Assets
+    this.num_total_assets =
+      this.assets_enemies.length + this.assets_friends.length
 
-    // initialize kaboom context
-    kaboom({
-      global: true
-    });
+    for (const enemy of this.assets_enemies) {
+      loadSprite(enemy, `/shooter/${enemy}.svg`).then(() => {
+        this.num_assets_loaded++
+
+        if (
+          this.num_assets_loaded == this.num_total_assets &&
+          this.game_started == false
+        ) {
+          // start the game
+          start("main")
+        }
+      })
+    }
+
+    for (const friend of this.assets_friends) {
+      loadSprite(friend, `/shooter/${friend}.svg`).then(() => {
+        this.num_assets_loaded++
+        if (
+          this.num_assets_loaded == this.num_total_assets &&
+          this.game_started == false
+        ) {
+          // start the game
+          start("main")
+        }
+      })
+    }
 
     // define a scene
     scene("main", () => {
+      console.log("main scene started")
+      this.game_started = true
 
-      // add a text at position (100, 100)
-      add([
-        text("kaboom", 32),
-        pos(100, 100),
-      ]);
+      layers(["game", "ui"], "game")
 
-      //loadRoot("/");
+      camIgnore(["ui"])
 
-      // load sprite
-      loadSprite("icons", "/shooter/icon-sprite-white.png").then(()=>{
-        add([
-          sprite("icons")
-        ])    
+      // Move Enemies down screen
+      action("enemy", t => {
+        t.move(0, t.speed * 1.5)
+        if (t.pos.y - t.height > height()) {
+          destroy(t)
+        }
       })
 
+      // Spawn enemies
+      this.spawnEnemy()
 
+      // Spawn player
+      this.player = add([
+        sprite("keyboard"),
+        area(),
+        pos(width() / 2, height() - 40),
+        origin("center")
+      ])
 
-    });
+      keyDown("left", () => {
+        this.player.move(-this.PLAYER_SPEED, 0)
+        if (this.player.pos.x < 0) {
+          this.player.pos.x = width()
+        }
+      })
 
-    // start the game
-    start("main");
-
+      keyDown("right", () => {
+        this.player.move(this.PLAYER_SPEED, 0)
+        if (this.player.pos.x > width()) {
+          this.player.pos.x = 0
+        }
+      })
+    })
   },
-  updated() {
-    
+  methods: {
+    spawnEnemy() {
+      console.log("Spawning an enemy!")
+
+      const name = choose(this.assets_enemies.filter(n => n))
+
+      add([
+        sprite(name),
+        area(),
+        pos(rand(0, width()), 0),
+        // health(this.ENEMY_HEALTH),
+        origin("bot"),
+        "enemy",
+        {
+          speed: rand(this.ENEMY_SPEED * 0.5, this.ENEMY_SPEED * 1.5)
+        }
+      ])
+
+      wait(0.5, this.spawnEnemy)
+    }
   },
-  beforeDestroy() {
-    
-  },
+  updated() {},
+  beforeDestroy() {}
 }
 </script>
 
-
-
-<style lang="scss">
-.shooter {
-
-}
-</style>
+<style lang="scss"></style>
