2b5a3e5299b3b176643f8969fdc3624b5d256989
Brendan Sheehan <15129375+feemcgill@users.noreply.github.com>
2022-10-27 13:41:07 -0700

    set the stage for gaming

---
+++ pages/folio/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="portfolio-page">
+    <ul class="portfolio-list">
+      <li>
+        <span class="title"> Goldteeth &amp; Co. </span>
+        <span class="year"> 2021 </span>
+        <span class="friend">Hugo &amp; Marie</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo"
+import ContactForm from "~/components/ContactForm"
+import ContactDeets from "~/components/ContactDeets"
+import Legal from "~/components/Legal"
+
+const data = [
+  {
+    name: "Goldteeth & Co.",
+    year: 2021,
+    description: "High end portfolio for a photography agency.",
+    friends: "Hugo & Marie",
+    category: "Headless Website",
+  },
+  {
+    name: "Coachella",
+  },
+]
+
+export default {
+  components: {},
+  mounted() {
+    var ztxt = new Ztextify(".txt", {
+      depth: "30px",
+      layers: 2,
+      fade: true,
+      direction: "forwards",
+      event: "pointer",
+      eventRotation: "5deg",
+    })
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,56 +1,7 @@
 <template>
-
-  <div class="home">
-
-    <div class="stage">
-
-      <figure draggable="true" class="logo" />
-
-      <figure draggable="true" class="nuthin" />
-
-    </div>
-
-  </div>
-
+  <div class="home"></div>
 </template>
 
-<script>
+<script></script>
 
-</script>
-
-<style lang="scss" scoped>
-.home {
-  background: $flair;
-  height: 100vh;
-  width: 100vw;
-  position: fixed;
-}
-.stage {
-  background: $white;
-  mix-blend-mode: screen;
-  position: absolute;
-  height: 100vh;
-  width: 100vw;
-  figure {
-    position: absolute;
-    background-repeat: no-repeat;
-    /* background-color: yellow; */
-  }
-  .logo {
-    width: 50vh;
-    padding-bottom: 12%;
-    top: 30px;
-    left: 100px;
-    background-image: url("~assets/graphix/devgru-logo.svg");
-  }
-
-  .nuthin {
-    width: 20vh;
-    padding-bottom: 22%;
-    top: 300px;
-    left: 700px;
-    background-image: url("~assets/graphix/nuthin.svg");
-  }
-}
-</style>
-
+<style lang="scss" scoped></style>
