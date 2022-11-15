8ef2abfcaac76bc03adbb3574767787d8c1f8ce1
Brendan Sheehan <15129375+feemcgill@users.noreply.github.com>
2022-11-14 16:37:46 -0800

    genius solution to prevent game from stretching

--- components/ShooterGame.vue
+++ components/ShooterGame.vue
@@ -605,8 +605,7 @@ export default {
     position: absolute;
     top: 0;
     left: 0;
-    // width: 500px;
-    // height: 500px;
+    object-fit: contain;
     width: 100%;
     height: 100%;
   }
