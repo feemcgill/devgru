b77ab7c36491afe822182b5d8d52d70660d30d65
Brendan Sheehan <15129375+feemcgill@users.noreply.github.com>
2023-02-07 10:16:36 -0800

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ export default {
       { name: "format-detection", content: "telephone=no" },
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "icon", type: "image/x-icon", href: "/favicon.svg" },
       {
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css?family=Nanum+Gothic+Coding:400,700",
